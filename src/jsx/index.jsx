8d49e7357d490381f2fdb37c8441e793f787f491
seaman <semen.bko@ya.ru>
2015-03-06 23:53:16 +0300

    bootstrap manipul

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,3 +1,34 @@
 // Non React
 var tabs = require('./ui/tabs.jsx');
 
+// React lorem carousel
+var React = require('react');
+
+var Carousel = require('react-bootstrap/lib/Carousel');
+var CarouselItem = require('react-bootstrap/lib/CarouselItem');
+
+
+var CarouselInstance = React.createClass({
+	render: function(){
+		return (
+			<Carousel>
+				<CarouselItem>
+					<div className='carousel-caption'>
+						<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</h3>
+						<p>Someone famous in Source Title</p>
+					</div>
+					<div className='carousel-caption'>
+						<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</h3>
+						<p>Someone famous in Source Title</p>
+					</div>
+					<div className='carousel-caption'>
+						<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</h3>
+						<p>Someone famous in Source Title</p>
+					</div>
+				</CarouselItem>
+			</Carousel>
+			);
+	}
+});
+
+React.render(<CarouselInstance />, document.querySelector('.lorem-carousel .container'));
